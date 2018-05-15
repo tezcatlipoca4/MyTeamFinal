01f5bc49276bbade1fa8bf4c9bea8ff7dab76d36
tezcatlipoca4 <stratosvrachiolias@gmail.com>
2018-05-15 16:13:54 +0300

    asyncLoaderImplemented

--- MyTeam/MyTeam/MainPage.xaml.cs
+++ MyTeam/MyTeam/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,26 @@ namespace MyTeam
         {
             InitializeComponent();
 
-            List<string> list = new List<string> {"Κεντρική Σελίδα", "Ρυθμισεις", "Σχετικά με την Konstraction"};
-
+            List<string> list = new List<string> { "Κεντρική Σελίδα", "Ρυθμισεις", "Σχετικά με την Konstraction" };
             listView.ItemsSource = list;
+
+            //Todo: Έλεγχος αν είναι η πρώτη φορά που τρέχει η εφαρμογή να πηγαίνει στις ρυθμίσεις με την επιλογή για feed απενεργοποιημένη
+            //Διαφορετικά πηγαίνει κανονικά στο feed
+
+            //Δεν εχουμε ομάδα
+            if (SettingsPage.TeamChosen == string.Empty)
+            {
+                navigationDrawer.ContentView = new SettingsPage().Content;
+                headerLabel.Text = "Ρυθμίσεις";
+
+                navigationDrawer.IsEnabled = false;
+
+            }
+            else
+            {
+                navigationDrawer.ContentView = new RssFeedPage().Content;
+            }
+
         }
 
         private void HamburgerButton_OnClicked(object sender, EventArgs e)
@@ -22,10 +40,9 @@ namespace MyTeam
             navigationDrawer.ToggleDrawer();
         }
 
+
         private void ListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            //κλέινουμε το menu
-            navigationDrawer.ToggleDrawer();
 
             //Με την επιλογή ενός αντικειμένου από το μενου
             switch (e.SelectedItem.ToString())
@@ -40,7 +57,11 @@ namespace MyTeam
 
                     break;
             }
+
+            //Αλλάζουμε την επικεφαλίδα ανάλογα με την επιλογή
             headerLabel.Text = e.SelectedItem.ToString();
+            //κλέινουμε το menu
+            navigationDrawer.ToggleDrawer();
         }
     }
 }
