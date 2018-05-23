4acb305b76627731c7a36e8e2887a47d23e7e573
Konstantinos Giannopoulos <kostaswipe@gmail.com>
2018-05-23 11:18:13 +0300

    BackButton Minor Changes.

--- MyTeam/MyTeam/MainPage.xaml.cs
+++ MyTeam/MyTeam/MainPage.xaml.cs
@@ -12,9 +12,19 @@ namespace MyTeam
         {
             InitializeComponent();
 
-            CheckConnectionAndNavigateToContent(new RssFeedPage().Content);            
-            backButton.IsVisible = false;
+            // Έλεγχος εάν η συσκευή Android διαθέτει Software Navigation Keys
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                bool hasNavigationBar = DependencyService.Get<App.IHasHardwareKeys>().IsNavigationBarAvailable();
+                screenHeight.Height = hasNavigationBar ? 450 : 500;
+            }
+            else { screenHeight.Height = 500; } // Fixed height εάν είναι iOS. Θα δούμε την συμπεριφορά του αργότερα
 
+            // Έλεγχος σύνδεσης κατά την εκκίνηση
+            CheckConnectionAndNavigateToContent(new RssFeedPage().Content);            
+            backButton.IsVisible = false; // Hidden by default
+
+            // Δημιουργία sidebar menu
             List<string> menuList = new List<string>
             {
                 "Ειδήσεις Ομάδας",
@@ -25,26 +35,20 @@ namespace MyTeam
                 "Ρυθμισεις",
                 "Σχετικά με την εφαρμογή"
             };
-            listView.ItemsSource = menuList;
-
-            // Έλεγχος εάν η συσκευή Android διαθέτει Software Navigation Keys
-            if (Device.RuntimePlatform == Device.Android)
-            {
-                bool hasNavigationBar = DependencyService.Get<App.IHasHardwareKeys>().IsNavigationBarAvailable();
-                screenHeight.Height = hasNavigationBar ? 450 : 500;
-            }
-            else { screenHeight.Height = 500; } // Fixed height εάν είναι iOS. Θα δούμε την συμπεριφορά του αργότερα
-
+            listView.ItemsSource = menuList; // Bind στο ListView
+           
             //Todo: Έλεγχος αν είναι η πρώτη φορά που τρέχει η εφαρμογή να πηγαίνει στις ρυθμίσεις με την επιλογή για feed απενεργοποιημένη
             //Διαφορετικά πηγαίνει κανονικά στο feed
 
         }
         
+        // Πάνω αριστερά button
         private void HamburgerButton_OnClicked(object sender, EventArgs e)
         {
             navigationDrawer.ToggleDrawer();
         }
 
+        // Πάνω δεξιά button
         private void BackButton_Clicked(object sender, EventArgs e)
         {
             listView.SelectedItem = "Ειδήσεις Ομάδας";
@@ -55,8 +59,7 @@ namespace MyTeam
         {
             if (App.IsDeviceConnected())
             {
-                navigationDrawer.ContentView = contentView;
-                backButton.IsVisible = false;
+                navigationDrawer.ContentView = contentView;                
             }
             else
             {
@@ -69,6 +72,7 @@ namespace MyTeam
             }
         }
 
+        // Sidebar menu on item selected actions
         private void ListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             //Με την επιλογή ενός αντικειμένου από το μενου
@@ -76,6 +80,7 @@ namespace MyTeam
             {
                 case "Ειδήσεις Ομάδας":
                     CheckConnectionAndNavigateToContent(new RssFeedPage().Content);
+                    backButton.IsVisible = false;
                     break;
 
 
