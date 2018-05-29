41f7a2372450d4e8359ea44901f0bb3ecc3feddc
konGiann <kostaswipe@gmail.com>
2018-05-29 11:33:37 +0300

    Random Loading RSS messages

--- MyTeam/MyTeam.Android/Resources/Resource.designer.cs
+++ MyTeam/MyTeam.Android/Resources/Resource.designer.cs
@@ -2348,26 +2348,26 @@ namespace MyTeam.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
+			// aapt resource value: 0x7f020134
+			public const int avd_hide_password_1 = 2130837812;
+			
 			// aapt resource value: 0x7f020135
-			public const int avd_hide_password_1 = 2130837813;
+			public const int avd_hide_password_2 = 2130837813;
 			
 			// aapt resource value: 0x7f020136
-			public const int avd_hide_password_2 = 2130837814;
-			
-			// aapt resource value: 0x7f020137
-			public const int avd_hide_password_3 = 2130837815;
+			public const int avd_hide_password_3 = 2130837814;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
+			// aapt resource value: 0x7f020137
+			public const int avd_show_password_1 = 2130837815;
+			
 			// aapt resource value: 0x7f020138
-			public const int avd_show_password_1 = 2130837816;
+			public const int avd_show_password_2 = 2130837816;
 			
 			// aapt resource value: 0x7f020139
-			public const int avd_show_password_2 = 2130837817;
-			
-			// aapt resource value: 0x7f02013a
-			public const int avd_show_password_3 = 2130837818;
+			public const int avd_show_password_3 = 2130837817;
 			
 			// aapt resource value: 0x7f020056
 			public const int design_bottom_navigation_item_background = 2130837590;
@@ -3011,11 +3011,11 @@ namespace MyTeam.Droid
 			// aapt resource value: 0x7f02012b
 			public const int notification_icon_background = 2130837803;
 			
-			// aapt resource value: 0x7f020133
-			public const int notification_template_icon_bg = 2130837811;
+			// aapt resource value: 0x7f020132
+			public const int notification_template_icon_bg = 2130837810;
 			
-			// aapt resource value: 0x7f020134
-			public const int notification_template_icon_low_bg = 2130837812;
+			// aapt resource value: 0x7f020133
+			public const int notification_template_icon_low_bg = 2130837811;
 			
 			// aapt resource value: 0x7f02012c
 			public const int notification_tile_bg = 2130837804;
@@ -3030,13 +3030,10 @@ namespace MyTeam.Droid
 			public const int splash_screen = 2130837807;
 			
 			// aapt resource value: 0x7f020130
-			public const int splashTemp = 2130837808;
+			public const int tooltip_frame_dark = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int tooltip_frame_dark = 2130837809;
-			
-			// aapt resource value: 0x7f020132
-			public const int tooltip_frame_light = 2130837810;
+			public const int tooltip_frame_light = 2130837809;
 			
 			static Drawable()
 			{
--- MyTeam/MyTeam/RssFeedPage.xaml.cs
+++ MyTeam/MyTeam/RssFeedPage.xaml.cs
@@ -13,138 +13,165 @@ using DataRow = System.Data.DataRow;
 
 namespace MyTeam
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class RssFeedPage : ContentPage
-    {
-        public RssFeedPage()
-        {
-            InitializeComponent();
-                       
-            //Βάζουμε τα εικονίδια στα banner από την ομάδα που έχει επιλέξει ο χρήστης
-            LeftBannerTeamLogo.Source = RightBannerTeamLogo.Source =
-            ImageSource.FromResource("MyTeam.Assets.Images.teamLogos." + SettingsPage.TeamChosen + ".png");
-            teamLabel.Text = SettingsPage.TeamLabel;
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RssFeedPage : ContentPage
+	{
 
-            dataGrid.GridLoaded += DataGrid_OnGridLoaded;
+		#region Properties
+		// Όλα τα τυχαία μηνύματα
+		public string[] RandomMessagesArray = new string[]{
+			"Ισιώνουμε τις γραμμες",
+			"Κουρεύουμε το γκαζόν",
+			"Ταΐζουμε τον Μπίσεσβαρ",
+			"Προπονούμε τους δικαστές",
+			"Πετάμε χαρταετό"
 
+		};
 
-            pullToRefresh.Refreshing += PullToRefresh_Refreshing;            
-            
+		public Random randomNumber = new Random();
+		#endregion
 
-            pullToRefresh.Refreshing += PullToRefresh_Refreshing;
+		//Ctor
+		public RssFeedPage()
+		{
+			InitializeComponent();
 
-            dataGrid.GroupCaptionTextFormat = "Δημοσιεύθηκαν: {Key}";
-            dataGrid.GroupColumnDescriptions.Add(new GroupColumnDescription()
-            {
-                ColumnName = "PublishedDatetime",
-                Converter = new GroupConverterByDate()
+			//Βάζουμε τα εικονίδια στα banner από την ομάδα που έχει επιλέξει ο χρήστης
+			LeftBannerTeamLogo.Source = RightBannerTeamLogo.Source =
+			ImageSource.FromResource("MyTeam.Assets.Images.teamLogos." + SettingsPage.TeamChosen + ".png");
+			teamLabel.Text = SettingsPage.TeamLabel;
 
-            });
-
-
-        }
-        private void PullToRefresh_Refreshing(object sender, EventArgs e)
-        {
-            pullToRefresh.IsRefreshing = true;
-            App.CurrentLoadedRssModels.Clear();
-            dataGrid.ItemsSource = null;
-            LoadDataToGrid();
-
-            pullToRefresh.IsRefreshing = false;
-        }
-
-        private List<RssModel> GetRssModels()
-        {
-            //Αν υπήρχαν δεδομένα από πρίν τα φρτώνουμε απο εκεί
-            if (App.CurrentLoadedRssModels.Count > 0)
-                return App.CurrentLoadedRssModels;
-            //Ο πίνακας FilteredBy... έχει φιξ τιμές και δεν χρειάζεται να τον παίρνουμε σαν όρισμα, αλλά τον βάζουμε κατευθείαν μέσα στη μέθοδο
-            List<RssModel> combinedResults = new List<RssModel>();
-
-            foreach (DataRow row in App.FilteredByTeamAndSiteDataTable.Rows)
-            {
-                Device.BeginInvokeOnMainThread(() => ActivityStatusLabel.Text = "Παρακαλώ περιμένετε...\nΦόρτωση ειδήσεων από το\n" + row["siteName"]);
-                Device.BeginInvokeOnMainThread(() => loadingActivitySiteImage.Source = ImageSource.FromResource("MyTeam.Assets.Images.siteLogos." + row["siteName"] + ".png"));
-
-                //Για κάθε ένα site βάζουμε τις τιμές στο combinedResults
-                //TODO: Να επιλέγει ο χρήστης από 5-15 άρθρα από κάθε σελίδα
-                List<RssModel> tempResults = GetRssFeed(row["rssType"].ToString(), row["url"].ToString(),
-                    row["siteName"].ToString(), SettingsPage.NumberOfRssFeedItems);
-                combinedResults.AddRange(tempResults);
-            }
-
-            //Ενημερώνουμε την στιγμή που τραβήξαμε τα δεδομένα
-            App.LastLoadedDateTime = DateTime.Now;
-            return combinedResults;
-        }
-
-        public async void LoadDataToGrid()
-        {
-            //Εμφανίζουμε το stacklayout με το status φόρτωσης δεδομένων
-            LoadingStatusStackLayout.IsVisible = true;
-
-            App.CurrentLoadedRssModels = await Task.Run(() => GetRssModels());
-            dataGrid.ItemsSource = new ObservableCollection<RssModel>(App.CurrentLoadedRssModels);
-            FooterLabel.Text = "Τελευταία ενημέρωση: " + App.LastLoadedDateTime.ToString("dd/MM/yy - HH:mm");
-            FooterLabel.HorizontalTextAlignment = TextAlignment.Center;
-
-            LoadingStatusStackLayout.IsVisible = false;
-        }
+			dataGrid.GridLoaded += DataGrid_OnGridLoaded;
 
 
-        //Επιστρέφει τα πιο πρόσφατα feeds βάσει των τιμων που δίνονται
-        public List<RssModel> GetRssFeed(string rssType, string url, string siteName, int numberOfItems)
-        {
-            //todo:Αν το feed δεν ερχεται σωστα να εημερώνεται ο χρήστης και να αφαιρείται 
+			pullToRefresh.Refreshing += PullToRefresh_Refreshing;
 
-            //Δημιουργούμε το XDocument το οποίο είναι κοινό ανεξαιρέτως του τύπου δομής RSS/Atom
-            var xDocument = XDocument.Load(url);
 
-            switch (rssType)
-            {
-                case "RSS":
-                    return (from feed in xDocument.Descendants("item")
-                            select new RssModel
-                            {
-                                SiteLogo = ImageSource.FromResource("MyTeam.Assets.Images.siteLogos." + siteName + ".png"),
-                                Title = feed.Element("title").Value,
-                                Url = feed.Element("link").Value,
-                                PublishedDatetime = DateTime.Parse(feed.Element("pubDate").Value)
-                            }).Take(numberOfItems).ToList();
+			pullToRefresh.Refreshing += PullToRefresh_Refreshing;
 
-                //Μιας και έχουμε μόνο δύο περιπτώσεις αν δεν είναι RSS θα είναι Atom οπότε μποροπυμε να το αφήσουμε στο default
-                default:
-                    return (from item in xDocument.Root.Elements().Where(i => i.Name.LocalName == "entry")
-                            select new RssModel
-                            {
-                                SiteLogo = ImageSource.FromResource("MyTeam.Assets.Images.siteLogos." + siteName + ".png"),
-                                Url = item.Elements().First(i => i.Name.LocalName == "link").Attribute("href").Value,
-                                Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
-                                PublishedDatetime =
-                                    DateTime.Parse(item.Elements().First(i => i.Name.LocalName == "updated").Value)
-                            }).Take(numberOfItems).ToList();
-            }
-        }
+			dataGrid.GroupCaptionTextFormat = "Δημοσιεύθηκαν: {Key}";
+			dataGrid.GroupColumnDescriptions.Add(new GroupColumnDescription()
+			{
+				ColumnName = "PublishedDatetime",
+				Converter = new GroupConverterByDate()
 
-        //Στο κλικ του χρήστη ανοίγουμε το url που επέλεξε στον browser που χρησιμοποιεί
-        private void DataGrid_OnGridTapped(object sender, GridTappedEventArgs e)
-        {
-            Device.OpenUri(new Uri(dataGrid.GetCellValue(e.RowData, "Url").ToString()));
-        }
+			});
 
-        public bool IsDeviceConnected()
-        {
-            return CrossConnectivity.Current.IsConnected;
-        }
+		}
 
-        private void DataGrid_OnGridLoaded(object sender, GridLoadedEventArgs e)
-        {
-            if (IsDeviceConnected())
+		#region Methods
+		private void PullToRefresh_Refreshing(object sender, EventArgs e)
+		{
+			pullToRefresh.IsRefreshing = true;
+			App.CurrentLoadedRssModels.Clear();
+			dataGrid.ItemsSource = null;
+			LoadDataToGrid();
+
+			pullToRefresh.IsRefreshing = false;
+		}
+
+		private List<RssModel> GetRssModels()
+		{
+			//Αν υπήρχαν δεδομένα από πρίν τα φρτώνουμε απο εκεί
+			if (App.CurrentLoadedRssModels.Count > 0)
+				return App.CurrentLoadedRssModels;
+			//Ο πίνακας FilteredBy... έχει φιξ τιμές και δεν χρειάζεται να τον παίρνουμε σαν όρισμα, αλλά τον βάζουμε κατευθείαν μέσα στη μέθοδο
+			List<RssModel> combinedResults = new List<RssModel>();
+
+			foreach (DataRow row in App.FilteredByTeamAndSiteDataTable.Rows)
+			{
+				Device.BeginInvokeOnMainThread(() => ActivityStatusLabel.Text = "Παρακαλώ περιμένετε...\nΦόρτωση ειδήσεων από το\n" + row["siteName"]);
+				Device.BeginInvokeOnMainThread(() => loadingActivitySiteImage.Source = ImageSource.FromResource("MyTeam.Assets.Images.siteLogos." + row["siteName"] + ".png"));
+				Device.BeginInvokeOnMainThread(GenerateRandomLoadingText);
+
+
+				//Για κάθε ένα site βάζουμε τις τιμές στο combinedResults
+				//TODO: Να επιλέγει ο χρήστης από 5-15 άρθρα από κάθε σελίδα
+				List<RssModel> tempResults = GetRssFeed(row["rssType"].ToString(), row["url"].ToString(),
+					row["siteName"].ToString(), SettingsPage.NumberOfRssFeedItems);
+				combinedResults.AddRange(tempResults);
+			}
+
+			//Ενημερώνουμε την στιγμή που τραβήξαμε τα δεδομένα
+			App.LastLoadedDateTime = DateTime.Now;
+			return combinedResults;
+		}
+
+		public async void LoadDataToGrid()
+		{
+			//Εμφανίζουμε το stacklayout με το status φόρτωσης δεδομένων
+			LoadingStatusStackLayout.IsVisible = true;
+
+			App.CurrentLoadedRssModels = await Task.Run(() => GetRssModels());
+			dataGrid.ItemsSource = new ObservableCollection<RssModel>(App.CurrentLoadedRssModels);
+			FooterLabel.Text = "Τελευταία ενημέρωση: " + App.LastLoadedDateTime.ToString("dd/MM/yy - HH:mm");
+			FooterLabel.HorizontalTextAlignment = TextAlignment.Center;
+
+			LoadingStatusStackLayout.IsVisible = false;
+		}
+
+		//Επιστρέφει τα πιο πρόσφατα feeds βάσει των τιμων που δίνονται
+		public List<RssModel> GetRssFeed(string rssType, string url, string siteName, int numberOfItems)
+		{
+			//todo:Αν το feed δεν ερχεται σωστα να εημερώνεται ο χρήστης και να αφαιρείται 
+
+			//Δημιουργούμε το XDocument το οποίο είναι κοινό ανεξαιρέτως του τύπου δομής RSS/Atom
+			var xDocument = XDocument.Load(url);
+
+			switch (rssType)
+			{
+				case "RSS":
+					return (from feed in xDocument.Descendants("item")
+							select new RssModel
+							{
+								SiteLogo = ImageSource.FromResource("MyTeam.Assets.Images.siteLogos." + siteName + ".png"),
+								Title = feed.Element("title").Value,
+								Url = feed.Element("link").Value,
+								PublishedDatetime = DateTime.Parse(feed.Element("pubDate").Value)
+							}).Take(numberOfItems).ToList();
+
+				//Μιας και έχουμε μόνο δύο περιπτώσεις αν δεν είναι RSS θα είναι Atom οπότε μποροπυμε να το αφήσουμε στο default
+				default:
+					return (from item in xDocument.Root.Elements().Where(i => i.Name.LocalName == "entry")
+							select new RssModel
+							{
+								SiteLogo = ImageSource.FromResource("MyTeam.Assets.Images.siteLogos." + siteName + ".png"),
+								Url = item.Elements().First(i => i.Name.LocalName == "link").Attribute("href").Value,
+								Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
+								PublishedDatetime =
+									DateTime.Parse(item.Elements().First(i => i.Name.LocalName == "updated").Value)
+							}).Take(numberOfItems).ToList();
+			}
+		}
+
+		//Στο κλικ του χρήστη ανοίγουμε το url που επέλεξε στον browser που χρησιμοποιεί
+		private void DataGrid_OnGridTapped(object sender, GridTappedEventArgs e)
+		{
+			Device.OpenUri(new Uri(dataGrid.GetCellValue(e.RowData, "Url").ToString()));
+		}
+
+		// Έλεγχος σύνδεσης συσκευή στο ίντερνετ
+		public bool IsDeviceConnected()
+		{
+			return CrossConnectivity.Current.IsConnected;
+		}
+
+		private void DataGrid_OnGridLoaded(object sender, GridLoadedEventArgs e)
+		{
+			if (IsDeviceConnected())
 			{
 				LoadDataToGrid();
 			}
-                
+		}
 
-        }
-    }
+		// Τυχαίο μήνυμα κατά την φόρτωση των RSS
+		void GenerateRandomLoadingText()
+		{
+			int randomIndex = randomNumber.Next(0, RandomMessagesArray.Length);
+			LoadingRandomText.Text = RandomMessagesArray[randomIndex];
+		}
+        #endregion
+
+
+	}
 }
