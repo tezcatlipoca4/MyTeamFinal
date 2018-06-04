7b6e0f927ad3ae415bfe934a400580f94fd94125
tezcatlipoca4 <stratosvrachiolias@gmail.com>
2018-06-04 13:53:20 +0300

    NewRss

--- MyTeam/MyTeam/App.xaml.cs
+++ MyTeam/MyTeam/App.xaml.cs
@@ -106,6 +106,8 @@ namespace MyTeam
             table.Columns.Add("rssType", typeof(string));
             table.Columns.Add("url", typeof(string));
 
+            /*
+
             //Γενικές Ειδήσεις
             table.Rows.Add("general", "Γενικές Ειδήσεις", "Contra", "Atom", "http://www.contra.gr/latest/?widget=rssfeed&view=feed&contentId=1169269");
             table.Rows.Add("general", "Γενικές Ειδήσεις", "Gazzetta", "RSS", "http://www.gazzetta.gr/rssfeeds/allnewsfeed");
@@ -163,6 +165,61 @@ namespace MyTeam
                 "http://www.sport24.gr/football/omades/paok/?widget=rssfeed&view=feed&contentId=174866");
             table.Rows.Add("paok", "ΠΑΟΚ", "SportFM", "Html", "http://www.sport-fm.gr/tag/paok");
             table.Rows.Add("paok", "ΠΑΟΚ", "SDNA", "", "http://www.sdna.gr/teams/paok");
+            */
+
+            table.Rows.Add("aek", "ΑΕΚ", "AEK365", "RSS", "http://feeds.feedburner.com/aek365gr-aek");
+            table.Rows.Add("aek", "ΑΕΚ", "Contra", "Atom", "http://www.contra.gr/soccer/aek/?widget=rssfeed&view=feed");
+            table.Rows.Add("aek", "ΑΕΚ", "Gazzetta", "RSS", "http://www.gazzetta.gr/taxonomy/term/1001/all/feed");
+            table.Rows.Add("aek", "ΑΕΚ", "NovaSports", "RSS", "http://www.novasports.gr/sys/novasports/RssFeed/GetFeed?type=2&id=16673&languageID=1");
+            table.Rows.Add("aek", "ΑΕΚ", "OnSports", "RSS", "http://www.onsports.gr/omades/aek?format=feed&type=rss");
+            table.Rows.Add("aek", "ΑΕΚ", "Sport24", "Atom", "http://www.sport24.gr/football/omades/aek/?widget=rssfeed&view=feed&contentId=174866");
+            table.Rows.Add("aek", "ΑΕΚ", "SportFM", "Html", "http://www.sport-fm.gr/tag/aek");
+            table.Rows.Add("general", "Γενικές Ειδήσεις", "Contra", "Atom", "http://www.contra.gr/latest/?widget=rssfeed&view=feed&contentId=1169269");
+            table.Rows.Add("general", "Γενικές Ειδήσεις", "Gazzetta", "RSS", "http://www.gazzetta.gr/rssfeeds/allnewsfeed");
+            table.Rows.Add("general", "Γενικές Ειδήσεις", "NovaSports", "RSS", "http://www.novasports.gr/sys/novasports/RssFeed/GetFeed?type=999&id=1&languageID=1");
+            table.Rows.Add("general", "Γενικές Ειδήσεις", "OnSports", "RSS", "https://www.onsports.gr/latest-news?format=feed");
+            table.Rows.Add("general", "Γενικές Ειδήσεις", "SDNA", "RSS", "http://www.sdna.gr/latest.xml");
+            table.Rows.Add("general", "Γενικές Ειδήσεις", "Sport24", "Atom", "http://www.sport24.gr/latest/?widget=rssfeed&view=feed&contentId=174866");
+            table.Rows.Add("general", "Γενικές Ειδήσεις", "SportFM", "Html", "http://www.sport-fm.gr/archive/latest/");
+            table.Rows.Add("osfp", "Ολυμπιακός", "Contra", "Atom", "http://www.contra.gr/soccer/olympiacos/?widget=rssfeed&view=feed");
+            //table.Rows.Add("osfp", "Ολυμπιακός", "Gayros", "RSS", "http://gavros.gr/rss?projection=370");
+            table.Rows.Add("osfp", "Ολυμπιακός", "Gazzetta", "RSS", "http://www.gazzetta.gr/taxonomy/term/1014/all/feed");
+            table.Rows.Add("osfp", "Ολυμπιακός", "NovaSports", "RSS", "http://www.novasports.gr/sys/novasports/RssFeed/GetFeed?type=2&id=16641&languageID=1");
+            table.Rows.Add("osfp", "Ολυμπιακός", "OnSports", "RSS", "http://www.onsports.gr/omades/olympiakos?format=feed&type=rss");
+            table.Rows.Add("osfp", "Ολυμπιακός", "Sport24", "Atom", "http://www.sport24.gr/football/omades/olympiakos/?widget=rssfeed&view=feed&contentId=174866");
+            table.Rows.Add("panathinaikos", "Παναθηναϊκός", "Contra", "Atom", "http://www.contra.gr/soccer/panathinaikos/?widget=rssfeed&view=feed");
+            table.Rows.Add("panathinaikos", "Παναθηναϊκός", "Gazzetta", "RSS", "http://www.gazzetta.gr/taxonomy/term/1016/all/feed");
+            table.Rows.Add("panathinaikos", "Παναθηναϊκός", "NovaSports", "RSS", "http://www.novasports.gr/sys/novasports/RssFeed/GetFeed?type=2&id=16625&languageID=1");
+            table.Rows.Add("panathinaikos", "Παναθηναϊκός", "OnSports", "RSS", "http://www.onsports.gr/omades/panathinaikos?format=feed&type=rss");
+            //table.Rows.Add("panathinaikos", "Παναθηναϊκός", "PrasinaNea", "RSS", "http://www.prasinanea.gr/feed/");
+            table.Rows.Add("panathinaikos", "Παναθηναϊκός", "Sport24", "Atom", "http://www.sport24.gr/football/omades/paok/?widget=rssfeed&view=feed&contentId=174866");
+            table.Rows.Add("paok", "ΠΑΟΚ", "Contra", "Atom", "http://www.contra.gr/soccer/paok/?widget=rssfeed&view=feed&contentId=1169269");
+            table.Rows.Add("paok", "ΠΑΟΚ", "Gazzetta", "RSS", "http://www.gazzetta.gr/taxonomy/term/1018/all/feed");
+            table.Rows.Add("paok", "ΠΑΟΚ", "inPAOK", "RSS", "http://inpaok.com/feed/");
+            table.Rows.Add("paok", "ΠΑΟΚ", "NovaSports", "RSS", "http://www.novasports.gr/sys/novasports/RssFeed/GetFeed?type=2&id=16657&languageID=1");
+            table.Rows.Add("paok", "ΠΑΟΚ", "OnSports", "RSS", "http://www.onsports.gr/omades/paok?format=feed&type=rss");
+            table.Rows.Add("paok", "ΠΑΟΚ", "SDNA", "Html", "http://www.sdna.gr/teams/paok");
+            table.Rows.Add("paok", "ΠΑΟΚ", "Sport24", "Atom", "http://www.sport24.gr/football/omades/paok/?widget=rssfeed&view=feed&contentId=174866");
+            table.Rows.Add("paok", "ΠΑΟΚ", "SportFM", "Html", "http://www.sport-fm.gr/tag/paok");
+            table.Rows.Add("osfp", "Ολυμπιακός", "Olympiacos.Org", "RSS", "http://www.olympiacos.org/feeds/social");
+            table.Rows.Add("atromitos", "Ατρόμητος", "Sport24", "Atom", "http://www.sport24.gr/football/omades/atromitos/?widget=rssfeed&view=feed&contentId=174866");
+            table.Rows.Add("atromitos", "Ατρόμητος", "NovaSports", "RSS", "http://www.novasports.gr/sys/novasports/RssFeed/GetFeed?type=2&id=17073&languageID=1");
+            table.Rows.Add("atromitos", "Ατρόμητος", "Gazzetta", "RSS", "http://www.gazzetta.gr/taxonomy/term/1008/all/feed");
+            table.Rows.Add("atromitos", "Ατρόμητος", "OnSports", "RSS", "https://www.onsports.gr/omades/atromitos?format=feed&type=rss");
+            table.Rows.Add("atromitos", "Ατρόμητος", "Contra", "Atom", "http://www.contra.gr/soccer/atromitos/?widget=rssfeed&view=feed");
+            table.Rows.Add("atromitos", "Ατρόμητος", "SportFM", "Html", "http://www.sport-fm.gr/tag/atromitos");
+            table.Rows.Add("atromitos", "Ατρόμητος", "SDNA", "Html", "http://www.sdna.gr/teams/atromitos");
+            table.Rows.Add("aris", "Άρης", "Sport24", "Atom", "http://www.sport24.gr/football/omades/aris/?widget=rssfeed&view=feed&contentId=174866");
+            table.Rows.Add("aris", "Άρης", "SportFM", "Html", "http://www.sport-fm.gr/tag/aris");
+            table.Rows.Add("aris", "Άρης", "NovaSports", "RSS", "http://www.novasports.gr/sys/novasports/RssFeed/GetFeed?type=2&id=16817&languageID=1");
+            table.Rows.Add("aris", "Άρης", "Gazzetta", "RSS", "http://www.gazzetta.gr/taxonomy/term/1006/all/feed");
+            table.Rows.Add("aris", "Άρης", "OnSports", "RSS", "https://www.onsports.gr/omades/aris?format=feed&type=rss");
+            table.Rows.Add("aris", "Άρης", "Contra", "Atom", "http://www.contra.gr/soccer/aris/?widget=rssfeed&view=feed");
+            table.Rows.Add("aris", "Άρης", "SDNA", "Html", "http://www.sdna.gr/teams/aris");
+            table.Rows.Add("aris", "Άρης", "Planetaris", "RSS", "http://feeds.feedburner.com/planetaris");
+            table.Rows.Add("paok", "ΠΑΟΚ", "PAOKFC", "RSS", "http://www.paokfc.gr/category/nea/feed/");
+            table.Rows.Add("atromitos", "Ατρόμητος", "AtromitosFC", "RSS", "http://www.atromitosfc.gr/el/news-el/news-el.feed?type=rss");
+            table.Rows.Add("aris", "Άρης", "ArisFC", "RSS", "http://arisfc.com.gr/index.php/enimerosi/category-items/4-news/2-last-news?format=feed&type=rss");
 
 
         }
