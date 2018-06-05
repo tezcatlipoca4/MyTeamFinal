fb37b0f2e8945da0e5447c412d299f3d55e8ed7f
tezcatlipoca4 <stratosvrachiolias@gmail.com>
2018-06-05 14:40:33 +0300

    program tv

---
+++ MyTeam/MyTeam/Models/Schedule.cs
@@ -0,0 +1,18 @@
+using System;
+namespace MyTeam.Models
+{
+    public class Schedule
+    {
+		public string Title { get; set; }
+
+		public string Time { get; set; }
+
+		public string NumberDate { get; set; }
+
+		public string MonthYear { get; set; }
+
+		public string Description { get; set; }
+
+		public string Channel { get; set; }
+    }
+}
